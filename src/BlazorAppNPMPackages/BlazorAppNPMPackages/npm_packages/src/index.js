94c7c45cfa540d443d544ec0bf5898fa86f2348c
M. Akif Tokatlioglu <akifmt@gmail.com>
2023-08-07 00:56:11 +0300

    added BlazorAppNPMPackages

---
+++ src/BlazorAppNPMPackages/BlazorAppNPMPackages/npm_packages/src/index.js
@@ -0,0 +1,49 @@
+﻿import Chart from 'chart.js/auto'
+
+const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+];
+
+window.LoadChart = function () {
+    new Chart(
+        document.getElementById('chart1'),
+        {
+            type: 'bar',
+            data: {
+                labels: data.map(row => row.year),
+                datasets: [
+                    {
+                        label: 'Data by year',
+                        data: data.map(row => row.count)
+                    }
+                ]
+            }
+        }
+    );
+
+    const ctx = document.getElementById('chart2');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Values',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
