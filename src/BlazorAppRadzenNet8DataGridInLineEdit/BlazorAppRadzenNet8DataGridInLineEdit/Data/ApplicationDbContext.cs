7c561534f6d0e11d41d145c344894103fdc05cb4
M. Akif Tokatlioglu <akifmt@gmail.com>
2025-06-16 00:08:26 +0300

    added BlazorAppRadzenNet8DataGridInLineEdit

---
+++ src/BlazorAppRadzenNet8DataGridInLineEdit/BlazorAppRadzenNet8DataGridInLineEdit/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlazorAppRadzenNet8DataGridInLineEdit.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppRadzenNet8DataGridInLineEdit.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<BlogPost> BlogPosts => Set<BlogPost>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
