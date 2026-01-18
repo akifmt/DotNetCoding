8d800e5e7d613fe6fbfee3480c04fead67bdca68
M. Akif Tokatlioglu <akifmt@gmail.com>
2026-01-18 22:17:35 +0300

    added BlazorAppNET10withMinimalAPI

---
+++ src/BlazorAppNET10withMinimalAPI/BlazorAppNET10withMinimalAPI/Program.cs
@@ -0,0 +1,36 @@
+using BlazorAppNET10withMinimalAPI.Components;
+
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var apiBaseUri = new Uri("https://localhost:6001"); // API URI
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = apiBaseUri });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
