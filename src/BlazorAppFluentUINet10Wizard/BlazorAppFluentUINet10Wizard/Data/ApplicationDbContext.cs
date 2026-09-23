6fd4fe8c2c2bf900f03d1fd1bb0b10039157dbf9
M. Akif Tokatlioglu <akifmt@gmail.com>
2026-09-23 21:49:59 +0300

    added BlazorAppFluentUINet10Wizard

---
+++ src/BlazorAppFluentUINet10Wizard/BlazorAppFluentUINet10Wizard/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlazorAppFluentUINet10Wizard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppFluentUINet10Wizard.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<BlogPost> BlogPosts => Set<BlogPost>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
